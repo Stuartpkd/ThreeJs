7341cc95ec59e5c8485d8e317b364d10b6aac793
stuartpkd <stuartparkerdesign@gmail.com>
2024-05-17 14:14:05 +0100

    Add cube to scene

--- main.js
+++ main.js
@@ -11,3 +11,7 @@ const geometry = new THREE.BoxGeometry();
 
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
