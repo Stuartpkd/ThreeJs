d92905412409e6dd865845196ed2427bcb321923
stuartpkd <stuartparkerdesign@gmail.com>
2024-05-17 14:15:32 +0100

    Add animation function for cube

--- main.js
+++ main.js
@@ -14,4 +14,13 @@ const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
 
